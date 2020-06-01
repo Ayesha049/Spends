3421861a09148ffb8686879fb7f1e474e57b3c39
Irfan <fahimirfan70@gmail.com>
2020-06-01 22:39:45 +0600

    Added datepicker

--- app/src/main/java/com/ayeshapp/spends/FragmentPeriodically.java
+++ app/src/main/java/com/ayeshapp/spends/FragmentPeriodically.java
@@ -1,17 +1,32 @@
 package com.ayeshapp.spends;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.DatePicker;
+import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import org.w3c.dom.Text;
+
+import java.util.Calendar;
+
 public class FragmentPeriodically extends Fragment {
     View view;
+    TextView startEditDate;
+    TextView finishEditDate;
+
     public FragmentPeriodically() {
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
     }
 
     @Nullable
@@ -20,4 +35,66 @@ public class FragmentPeriodically extends Fragment {
         view = inflater.inflate(R.layout.periodically_fragment, container, false);
         return view;
     }
+
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        startEditDate = view.findViewById(R.id.start_date);
+        startEditDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDatePicker(1);
+            }
+        });
+
+        finishEditDate = view.findViewById(R.id.finish_date);
+        finishEditDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDatePicker(2);
+            }
+        });
+    }
+
+    private void showDatePicker(int pos) {
+        DatePickerFragment date = new DatePickerFragment();
+        /**
+         * Set Up Current Date Into dialog
+         */
+        Calendar calender = Calendar.getInstance();
+        Bundle args = new Bundle();
+        args.putInt("year", calender.get(Calendar.YEAR));
+        args.putInt("month", calender.get(Calendar.MONTH));
+        args.putInt("day", calender.get(Calendar.DAY_OF_MONTH));
+        date.setArguments(args);
+        /**
+         * Set Call back to capture selected date
+         */
+        if(pos == 1){
+            date.setCallBack(ondatestart);
+        } else{
+            date.setCallBack(ondatefinish);
+
+        }
+        date.show(getFragmentManager(), "Date Picker");
+    }
+    DatePickerDialog.OnDateSetListener ondatestart = new DatePickerDialog.OnDateSetListener() {
+
+        public void onDateSet(DatePicker view, int year, int monthOfYear,
+                              int dayOfMonth) {
+
+            startEditDate.setText(String.valueOf(dayOfMonth) + "-" + String.valueOf(monthOfYear+1)
+                    + "-" + String.valueOf(year));
+        }
+    };
+
+    DatePickerDialog.OnDateSetListener ondatefinish = new DatePickerDialog.OnDateSetListener() {
+
+        public void onDateSet(DatePicker view, int year, int monthOfYear,
+                              int dayOfMonth) {
+
+            finishEditDate.setText(String.valueOf(dayOfMonth) + "-" + String.valueOf(monthOfYear+1)
+                    + "-" + String.valueOf(year));
+        }
+    };
+
 }
